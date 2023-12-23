a17409d2cef5360bcc0dc0425417e199565bd9be
Aaron Jheng <wentworth@outlook.com>
2023-12-23 02:28:00 +0000

    clash

---
+++ pkgs/by-name/cl/clash/package.nix
@@ -0,0 +1,43 @@
+{ lib
+, fetchFromGitHub
+, buildGoModule
+, testers
+, clash
+}:
+
+buildGoModule rec {
+  pname = "clash";
+  version = "1.18.0";
+
+  src = fetchFromGitHub {
+    owner = "aaronjheng";
+    repo = "clash";
+    rev = "v${version}";
+    hash = "sha256-LqjSPlPkR5sB4Z1pmpdE9r66NN7pwgE9GK4r1zSFlxs=";
+  };
+
+  vendorHash = "sha256-EWAbEFYr15RiJk9IXF6KaaX4GaSCa6E4+8rKL4/XG8Y=";
+
+  subPackages = [ "." ];
+
+  CGO_ENABLED = 0;
+
+  ldflags = [
+    "-s"
+    "-w"
+    "-X github.com/Dreamacro/clash/constant.Version=${version}"
+  ];
+
+  passthru.tests.version = testers.testVersion {
+    package = clash;
+    command = "clash -v";
+  };
+
+  meta = with lib; {
+    description = "A rule-based tunnel in Go";
+    homepage = "https://github.com/Dreamacro/clash";
+    license = licenses.gpl3Only;
+    maintainers = with maintainers; [ aaronjheng ];
+    mainProgram = "clash";
+  };
+}
